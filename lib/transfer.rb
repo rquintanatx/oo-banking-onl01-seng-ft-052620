809f3a5864f722414cb1523973fbf9e532123a78
Robert Quintana <robertquintanatx@gmail.com>
2020-06-16 05:33:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       @receiver.deposit(@amount)
       @status = "complete"
     else 
-      @status = "rejected"
+      @status = "Transaction rejected. Please check your account balance."
     end 
   end 
 end   
