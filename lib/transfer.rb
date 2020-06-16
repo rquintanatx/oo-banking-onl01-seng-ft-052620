b1568adc09884a9a9850157578d0a208978559c1
Robert Quintana <robertquintanatx@gmail.com>
2020-06-16 05:18:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,10 @@
 class Transfer
+  attr_accessor :status
   attr_reader :sender, :receiver
   
   def initialize(sender, reciever, amt)
     @sender = sender
     @receiver = reciever
+    @status = "pending"
   end
 end   
